1781850d552111d399e13a888910d4338cc075a7
gummipunkt <patrick@gummipunkt.eu>
2020-01-30 16:43:20 +0100

    updates

--- gui.py
+++ gui.py
@@ -1,28 +1,36 @@
 import PySimpleGUI as sg, random, string, hashlib, time, os, sqlite3, os.path, binascii
-salt = hashlib.sha256(os.urandom(256)).hexdigest().encode('ascii') # create salt
 
+salt = hashlib.sha256(os.urandom(256)).hexdigest().encode('ascii')  # create salt
 
 ##  THEME
 
 
 sg.theme("TanBlue")  # please make your windows colorful
 
-layout = [[sg.Text('Number of Characters'), sg.Text(size=(50,1), key='count')],
-          [sg.Slider(range=(1, 100), orientation='h', size=(20, 15), default_value=25, tick_interval=25)],
-          [sg.Text('Website URL'), sg.Text(size=(12,2), key='url')],
-          [sg.Input(key='url_input')],
-          [sg.Text('Username or E-mail address'), sg.Text(size=(12, 1), key='user')],
-          [sg.Input(key='user_input')],
-          [sg.Frame(layout=[
-              [sg.Checkbox('Numbers', size=(20, 1), default=True), sg.Checkbox('Letters', default=True)],
-              [sg.Checkbox('Special Character', size=(20, 1), default=True)]], title='Options',title_color='red')],
-          [sg.Button('Generate'), sg.Button('Close')],
-         ]
+tab1_layout = [[sg.Text('Number of Characters'), sg.Text(size=(50, 1), key='count')],
+               [sg.Slider(range=(1, 100), orientation='h', size=(20, 15), default_value=25, tick_interval=25)],
+               [sg.Text('Website URL'), sg.Text(size=(12, 2), key='url')],
+               [sg.Input(key='url_input')],
+               [sg.Text('Username or E-mail address'), sg.Text(size=(12, 1), key='user')],
+               [sg.Input(key='user_input')],
+               [sg.Frame(layout=[
+                   [sg.Checkbox('Numbers', size=(20, 1), default=True), sg.Checkbox('Letters', default=True)],
+                   [sg.Checkbox('Special Character', size=(20, 1), default=True)]], title='Options',
+                   title_color='red')],
+               [sg.Text('Your password:'), sg.Text(size=(30,1), key='password_output')],
+               [sg.Button('Generate'), sg.Button('Close')],
+               ]
+list_layout = [[sg.Text('hier gehts bald weiter'), sg.Text(size=(50, 1), key='count1')],
+
+               ]
+
+layout = [[sg.Text('merle-pwd - password manager', background_color='lightblue', justification='center', size=(30, 1), font=("Ubuntu", 25))],
+          [sg.TabGroup([[sg.Tab('Generate', tab1_layout), sg.Tab('List', list_layout)]])]]
 
 window = sg.Window("merle-pwd v0.2 - NO PRODUCTIVE USE! IT IS CRAP!", layout)
 
 while True:  # Event Loop
-    event, values = window.read()       # can also be written as event, values = window()
+    event, values = window.read()  # can also be written as event, values = window()
 
     if event is None or event == 'Exit':
         break
@@ -34,6 +42,7 @@ while True:  # Event Loop
         window['user'].update(values['user_input'])
         # above line can also be written without the update specified
 
+    print(values.get(0))
     count = values.get(0)
     url = values["url_input"]
     user = values["user_input"]
@@ -51,29 +60,28 @@ while True:  # Event Loop
     else:
         special_dec = ""
 
+
     def rand_pass(size):
         # Takes random choices from
         # ascii_letters and digits
-        generate_pass = ''.join([random.choice(letters_dec            +
-                                               numbers_dec            +
+        generate_pass = ''.join([random.choice(letters_dec +
+                                               numbers_dec +
                                                special_dec)
-                                for n in range(size)])
+                                 for n in range(size)])
 
         return generate_pass
-
 
 
     # Driver Code
     password = rand_pass(int(count))
 
-
     # password hash with hashlib and encode password utf8
     password_salt = hashlib.pbkdf2_hmac(
         "sha256",  # The hash digest algorithm for HMAC
-        password.encode("utf-8"), # Convert the password to bytes
+        password.encode("utf-8"),  # Convert the password to bytes
         salt,  # Provide the salt
         500000,  # 200.000 iterations
-        dklen = 256  # 64 byte key (32bit recommend, 64, 128, 256, 512 eg possible. standard: 65)
+        dklen=256  # 64 byte key (32bit recommend, 64, 128, 256, 512 eg possible. standard: 65)
     )
 
     password_salt = binascii.hexlify(password_salt)
@@ -93,11 +101,11 @@ while True:  # Event Loop
 
     user_storage = (salt + user_salt).decode('ascii')  # store salt and key
 
-    #salt_from_storage = storage[:64] # 32bit is the  salt
-#key_from_storage = storage[64:]
+    # salt_from_storage = storage[:64] # 32bit is the  salt
+    # key_from_storage = storage[64:]
 
     created = time.strftime("%m%d%Y%.%H%M%S")
-    userdata = [ (user_salt, password_salt, url, storage, created ) ]
+    userdata = [(user_salt, password_salt, url, storage, created)]
 
     if os.path.isfile("tresor.sql.db") == True:
         connection = sqlite3.connect("tresor_sql.db")  # create database
@@ -128,8 +136,26 @@ while True:  # Event Loop
         cursor.execute(sql_command)
         connection.commit()
         connection.close()
-    window.close()
 
-sg.Popup('Passwort generiert',
-         'Das Passwort lautet', password,
-         'und wurde erfolgreich in der Datenbank gespeichert!')
+        password_result = password
+
+        window['password_output'].update(password_result)
+
+        ## Export Database
+
+        connection = sqlite3.connect("tresor_sql.db")  # connect database
+        cursor = connection.cursor()
+
+        sql = "SELECT * FROM entries"
+
+        cursor.execute(sql)
+        for dsatz in cursor:
+            unique_id = dsatz[0]
+            user_email = dsatz[1]
+            password = dsatz[2]
+            url = dsatz[3]
+            storage = dsatz[4]
+            created = dsatz[5]
+        connection.close()
+
+window.close()
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 ###### Welcome to merle-pwd.
 
-    -
 
 merle-pwd will be a password manager to create and organize all of your passwords - for free.
 It's public domain. Forever. It will work on every device you've. merle-pwd will sync your
@@ -11,22 +10,29 @@ passwords, if you want with your self hosted on your server.
 **What works**
 - Password creation (in a basic way)
 - Storing into a sqlite3 database
-    - Dictonary: UniqueID, Timecode, User, Site, Key (Password Hash & Salt)
-    
-**What should work**
-- GUI
+    - Dictonary: UniqueID, Timecode, Password User (Password Hash & Salt), Site, Password Key (Password Hash & Salt)
 - Password creation only with ASCII/digits e.g.
+- Salt Hash Keys
+- Salt Hash User
+
+**Next**
+- Show the list of saved websites
+- Restore Passwords
+
+**Ongoing**
+- GUI
+
+**What should work**
 - How to: create a secure password
 - Remember to change an old password
 - 2FA 
 - Login into merle-pwd with Master Password and/or 2FA-Key, Fingerprint
 - App: Android, iOS
 - Desktop: macOS, Linux, Windows
-- Salt Hash Keys
-- Edit password
+- Edit passwords
 
 **Important**
-- Anyone has to take care, that the keys and files are safe. I can't do that. 
+- This isn't safe. Use a good password manager like 1Password, KeyPass, LastPass e.g.
 - This is a "how to learn coding with python". I tried it several times and I failed every single
 time. So I try it this way, perhaps it will work.
 
